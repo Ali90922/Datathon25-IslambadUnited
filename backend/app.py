adc0f1756a2eb04a5fcb1c51437846428528aa25
Ali Nawaz <150611589+Ali90922@users.noreply.github.com>
2025-03-01 15:32:25 -0600

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -201,4 +201,4 @@ def predict_from_text():
 # --------------------------------------------------------------------
 if __name__ == "__main__":
     # Make sure your server is open to external calls if you want outside access
-    app.run(host="0.0.0.0", port=6000, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
